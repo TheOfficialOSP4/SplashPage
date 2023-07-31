d6464e0468e388dd46e02fe290b2992e4c16bc9b
xjqiu28 <76609392+xjqiu28@users.noreply.github.com>
2023-07-31 19:56:00 -0400

    added formatting to middle logo and fixed navigation formatting

--- client/components/Command.jsx
+++ client/components/Command.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import '../styles/command.scss';
 
 const Command = () => {
 
+    //useState default false, changes to true if copied successfully
+    const [copied, setCopied] = useState(false);
+    const [clipboard, setClipboard] = useState('');
+    
+    // copies to clipboard when pressed
+    async function handleClick(){
+        const text = 'npm install shieldql';
+        // writes the text we initialized onto the clipboard
+        await navigator.clipboard.writeText(text);
+        //sets the copied value to true and makes the button change from copy -> copied!
+        return setCopied(true);
+    }
+
+    return (
+        <div className="clipboard">
+            <input type="text" value="@npm install shieldql" readOnly />
+            {copied ? <button>Copied!</button> : <button onClick={handleClick}>Copy</button>}
+        </div>
+    )
 }
 
 export default Command;
--- client/components/Team.jsx
+++ client/components/Team.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-
+// MAKE SURE TO COMMENT PLS 
+// import '../styles/team.scss';
 // Teammate
 import Teammate from './Teammate.jsx';
 const Team = () => {
