13f46868e6a7da19d17456486df9c6b43d9c8641
xjqiu28 <76609392+xjqiu28@users.noreply.github.com>
2023-08-03 12:20:43 -0400

    setup and planned team and teammate components for after lunch

--- client/components/Team.jsx
+++ client/components/Team.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 // MAKE SURE TO COMMENT PLS 
 // import '../styles/team.scss';
 // Teammate
 import Teammate from './Teammate.jsx';
 const Team = () => {
+    const [profiles, setProfiles] = [];
+    const names = ['Joie Zhang', 'Rodrigo Samour Calderon', 'Siful Siddiki', 'Simran Kaur', 'Xin Jin Qiu'];
+    const githubs = ['', '', '', '', ''];
+    const linkedins = ['', '', '', '', ''];
     
+    return (
+        <div>
+        </div>
+    )
 };
 export default Team;
--- client/components/Teammate.jsx
+++ client/components/Teammate.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 // import '../styles/teammate.scss';
-const Teammate = () => {
-    
+const Teammate = (props) => {
+    return (
+        <div>
+            
+        </div>
+    )
 };
 
 export default Teammate;
