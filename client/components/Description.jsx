fbf23cb370c953c2b4d3f6fab3dba7657427cf12
xjqiu28 <76609392+xjqiu28@users.noreply.github.com>
2023-08-03 11:12:06 -0400

    added description into splashPage and fixed some styling

---
+++ client/components/Description.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import '../styles/description.scss'
+
+//Parent Component: Mains.jsx
+// contains description of our app
+const Description = () => {
+    return (
+        <div className="description">
+            <h1>The lightweight graphQL library that
+                protects your queries through user
+                authentication, authorization, and query
+                sanitization.</h1>
+        </div>
+    )
+
+};
+
+export default Description;
--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -4,17 +4,19 @@ import Navigation from './Navigation.jsx';
 import Shieldql from './Shieldql.jsx'
 import Redirect from './Redirect.jsx';
 import Team from './Team.jsx';
+import Description from './Description.jsx';
 import '../styles/main.scss';
 
 //Parent Componenet: App.jsx
 // contains all the components of our application
-const Main = () =>{
+const Main = () => {
     return (
         <div className='grid-container'>
             <Navigation />
             <Shieldql />
+            <Description />
         </div>
-        
+
     )
 }
 
