3e314d7e615166c9cf57e1207f32bef0283a44bf
xjqiu28 <76609392+xjqiu28@users.noreply.github.com>
2023-08-03 18:58:53 -0400

    added refresh functionality when clicked on top left navigation items

--- client/components/LeftNavigation.jsx
+++ client/components/LeftNavigation.jsx
@@ -5,10 +5,11 @@ import '../styles/LeftNavigation.scss';
 //Parent Component: Navigation.jsx
 // contains logo image as well as the ShieldQL name on the top left of screen
 const LeftNavigation = () => {
+    
     return (
         <div className="left-nav">
-            <img className="logoImage" src={logo} alt="ShieldQL-Logo" />
-            <p>ShieldQL</p>
+            <img onClick={()=>{window.location.reload()}} className="logoImage" src={logo} alt="ShieldQL-Logo" />
+            <p onClick={()=>{window.location.reload()}}>ShieldQL</p>
         </div>
     )
 
