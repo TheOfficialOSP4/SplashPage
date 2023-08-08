d491d5a39cf3111edb6c8a1e589d5e935f0e3bb4
sifulsidd <sifulsiddiki@gmail.com>
2023-08-08 19:10:58 -0400

    Fixed all images

--- client/components/FirstFeature.jsx
+++ client/components/FirstFeature.jsx
@@ -11,14 +11,11 @@ const FirstFeature = () => {
           ensuring that only authenticated users can access certain parts of
           your API.
         </li>
-        {/* <li>this is test for 3a</li>
-        <li>this is test for 3b</li> */}
       </ul>
       </div>
-      <div className='images'>
-      {/* <img src='https://upload.wikimedia.org/wikipedia/commons/8/8b/Eo_circle_green_white_checkmark.svg' /> */}
-      <img src='https://www.indusface.com/wp-content/uploads/2016/11/How-does-SQLi-attack-works.png'/>
-      <img src='https://www.cisco.com/c/dam/assets/swa/img/anchor-info/what-is-user-authentication-628x353.png' />
+      <div className='img-div'>
+        <img src='https://www.indusface.com/wp-content/uploads/2016/11/How-does-SQLi-attack-works.png'/>
+        <img src='https://www.cisco.com/c/dam/assets/swa/img/anchor-info/what-is-user-authentication-628x353.png' />
       </div>
     </div>
   );
