34333dc35ab00af8514353a86e36c2a3dcf2a24c
rscalderon <127636148+rscalderon@users.noreply.github.com>
2024-09-14 15:38:50 -0400

    Convert App to TS (and merge with Main component)

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,25 @@
-import Main from './Main.jsx';
-import React from 'react';
+import Navigation from './Navigation.tsx';
+import Shieldql from './Shieldql.tsx';
+import Redirect from './Redirect.tsx';
+import Team from './Team.tsx';
+import Description from './Description.tsx';
+import Features from './Features.tsx';
+
+import '../styles/main.scss';
+
 import '../styles/app.scss';
 
 const App = () => {
-    return (
-        <div>
-            <Main />
-        </div>
-
-    )
-}
-
+  return (
+    <div className='grid-container'>
+      <Navigation />
+      <Shieldql />
+      <Description />
+      <Redirect />
+      <Team />
+      <Features />
+    </div>
+  );
+};
 
 export default App;
