842a96fcab08b113bec144529dbe9e675578161b
rscalderon <127636148+rscalderon@users.noreply.github.com>
2024-09-14 15:38:18 -0400

    update index file and html

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import App from './components/App';
-import { createRoot } from 'react-dom/client';
-
-const container = document.getElementById('root');
-const root = createRoot(container);
-
-root.render(<App />);
---
+++ src/index.tsx
@@ -0,0 +1,4 @@
+import App from './components/App';
+import { createRoot } from 'react-dom/client';
+
+createRoot(document.getElementById('root')!).render(<App />);
