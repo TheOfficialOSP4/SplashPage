64ac15d69d13b60c65101390bb0481a65ffa37bb
rscalderon <127636148+rscalderon@users.noreply.github.com>
2024-09-14 15:41:02 -0400

    migrate secondfeature

---
+++ src/components/SecondFeature.tsx
@@ -0,0 +1,26 @@
+import '../styles/secondFeature.scss';
+import login from '../assets/json.png';
+import secure_server from '../assets/secure_server.png';
+const SecondFeature = () => {
+  return (
+    <div className='second-feature'>
+      <div className='list'>
+        <ul>
+          <li>
+            With ShieldQL, you can define granular access controls for different
+            types and fields in your GraphQL schema. This way, you can control
+            what data each user can access based on their role and permissions.
+          </li>
+          {/* <li>this is test for 3a</li>
+        <li>this is test for 3b</li> */}
+        </ul>
+      </div>
+      <div>
+        <img src={secure_server} alt='secure_server image' />
+        <img src={login} alt='login image' />
+      </div>
+    </div>
+  );
+};
+
+export default SecondFeature;
