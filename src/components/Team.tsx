c159ee8b7d1070e1b980de04134ba2185444e477
rscalderon <127636148+rscalderon@users.noreply.github.com>
2024-09-14 15:59:19 -0400

    streamlined teams render logic

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,56 +1,59 @@
-import '../styles/team.scss';
+import Teammate from './Teammate';
 
 import joie from '../assets/joie.png';
 import rodrigo from '../assets/rodrigo.png';
 import simran from '../assets/simran.png';
 import xinjin from '../assets/xinjin.png';
 import siful from '../assets/siful.png';
-import Teammate from './Teammate';
+
+import '../styles/team.scss';
 
 const Team = () => {
-  // created an array for pictures paths, names of each individual, githubs and linkedins
-  const pictures = [joie, rodrigo, siful, simran, xinjin];
-  const names = [
-    'Joie Zhang',
-    'Rodrigo S. Calderon',
-    'Siful Siddiki',
-    'Simran Kaur',
-    'Xin Jin Qiu',
-  ];
-  const githubs = [
-    'https://github.com/joie-zhang',
-    'https://github.com/rscalderon',
-    'https://github.com/sifulsidd',
-    'https://github.com/simk209',
-    'https://github.com/xjqiu28',
-  ];
-  const linkedins = [
-    'https://www.linkedin.com/in/joie-zhang/',
-    'https://www.linkedin.com/in/rodrigosamourcalderon/',
-    'https://www.linkedin.com/in/siful-siddiki/',
-    'https://www.linkedin.com/in/simran-kaur-nyc/',
-    'https://www.linkedin.com/in/xinjinqiu/',
-  ];
-
-  // create an array that will contain a list of teammates
-  const profArray = [];
-
-  // loop over everything and pass in the information as props to teammate component
-  for (let i = 0; i < names.length; i++) {
-    profArray.push(
-      <Teammate
-        name={names[i]}
-        github={githubs[i]}
-        linkedin={linkedins[i]}
-        picture={pictures[i]}
-      />
-    );
-  }
+  const teammates = [
+    {
+      name: 'Joie Zhang',
+      github: 'https://github.com/joie-zhang',
+      linkedin: 'https://www.linkedin.com/in/joie-zhang/',
+      picture: joie,
+    },
+    {
+      name: 'Rodrigo S. Calderon',
+      github: 'https://github.com/rscalderon',
+      linkedin: 'https://www.linkedin.com/in/rodrigosamourcalderon/',
+      picture: rodrigo,
+    },
+    {
+      name: 'Siful Siddiki',
+      github: 'https://github.com/sifulsidd',
+      linkedin: 'https://www.linkedin.com/in/siful-siddiki/',
+      picture: simran,
+    },
+    {
+      name: 'Simran Kaur',
+      github: 'https://github.com/simk209',
+      linkedin: 'https://www.linkedin.com/in/simran-kaur-nyc/',
+      picture: xinjin,
+    },
+    {
+      name: 'Xin Jin Qiu',
+      github: 'https://github.com/xjqiu28',
+      linkedin: 'https://www.linkedin.com/in/xinjinqiu/',
+      picture: siful,
+    },
+  ].map(({ name, github, linkedin, picture }) => (
+    <Teammate
+      name={name}
+      github={github}
+      linkedin={linkedin}
+      picture={picture}
+      key={`${name}-${github}-${linkedin}`}
+    />
+  ));
 
   return (
     <div id='team'>
       <h1> Meet the Team </h1>
-      <div className='team'>{profArray}</div>
+      <div className='team'>{teammates}</div>
     </div>
   );
 };
